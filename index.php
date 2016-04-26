6c957753ce16b24342ac148761428cf085acc5f8
DanNeciu <DanNeciu>
2016-04-26 11:49:06 +0300

    added files

---
+++ index.php
@@ -0,0 +1,53 @@
+<html>
+<head>
+	<title>Timeline</title>
+	<link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,700&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
+	<link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<div id="main">
+		<div class="container">
+			<div class="col-md-12">
+				<h3>Timeline</h3>
+				<div class="history">
+					<div class="etapa par an1">
+						<h4>2000</h4>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam quo, vitae recusandae sit beatae quidem doloribus ipsa hic nisi temporibus voluptates sequi natus dolores maiores tempore, nam consequuntur minus, amet.</p>
+					</div>
+					<div class="etapa impar an2">
+						<h4>2006</h4>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam quo, vitae recusandae sit beatae quidem doloribus ipsa hic nisi</p>
+					</div>
+					<div class="clearfix"></div>
+					<div class="etapa par an3">
+						<h4>2007</h4>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam quo, vitae recusandae sit beatae quidem doloribus ipsa hic nisiLorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam quo, vitae recusandae sit beatae quidem doloribus ipsa hic nisiLorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam quo, vitae recusandae sit beatae quidem doloribus ipsa hic nisi</p>
+					</div>
+					<div class="etapa impar an4">
+						<h4>2008</h4>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam quo, vitae recusandae </p>
+					</div>
+					<div class="etapa impar an5">
+						<h4>2010</h4>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam quo, vitae recusandae </p>
+					</div>
+					<div class="etapa par an6">
+						<h4>2013</h4>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam quo, vitae recusandae </p>
+					</div>
+					<div class="etapa impar an7">
+						<h4>2014</h4>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam quo, vitae recusandae Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam quo, vitae recusandae </p>
+					</div>
+					<div class="etapa par an8">
+						<h4>2015</h4>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam quo, vitae recusandae </p>
+					</div>
+					<div class="clearfix"></div>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
